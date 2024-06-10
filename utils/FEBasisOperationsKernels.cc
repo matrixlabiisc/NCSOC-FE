e55ff1c0586b4509acde8b2a23e401ca4fa75ebb
Nikhil Kodali <nikhilkodali@iisc.ac.in>
2024-06-10 21:51:34 +0530

    Fix interpolate

--- utils/FEBasisOperationsKernels.cc
+++ utils/FEBasisOperationsKernels.cc
@@ -111,10 +111,12 @@ namespace dftfe
               iCell, std::min(d_nCells, iCell + d_cellsBlockSize)));
           interpolateKernel(
             tempCellNodalData.data(),
-            quadratureValues +
-              (iCell - cellRange.first) * d_nQuadsPerCell[d_quadratureIndex],
+            quadratureValues + (iCell - cellRange.first) *
+                                 d_nQuadsPerCell[d_quadratureIndex] *
+                                 d_nVectors,
             quadratureGradients + (iCell - cellRange.first) *
-                                    d_nQuadsPerCell[d_quadratureIndex] * 3,
+                                    d_nQuadsPerCell[d_quadratureIndex] * 3 *
+                                    d_nVectors,
             std::pair<unsigned int, unsigned int>(
               iCell, std::min(d_nCells, iCell + d_cellsBlockSize)));
         }
